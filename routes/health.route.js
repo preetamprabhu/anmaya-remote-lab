cf1d310abeb04d975a5433f9736b6bdbaab20952
Uttam <preetam11prabhu@gmail.com>
2025-03-29 12:55:58 +0530

    remove healthroute

--- routes/health.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const HealthController = require('../controllers/health.Controller');
-
-router.get('/health', HealthController.checkHealth);
-router.get('/send-message', HealthController.sendMessage);
-
-module.exports = router;
